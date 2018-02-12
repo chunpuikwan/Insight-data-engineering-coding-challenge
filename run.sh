2b943ab83a6e049807c1e50d140b66dcdfcc9cd5
chunpuikwan <36138579+chunpuikwan@users.noreply.github.com>
2018-02-12 17:02:12 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@
 # Make sure the python files are executable
 chmod a+x src/script.py
 # Execute the following command from the root directory
-python src/words_tweeted.py input/itcont.txt input/percentile.txt output/repeat_donors.txt
+python src/script.py input/itcont.txt input/percentile.txt output/repeat_donors.txt
