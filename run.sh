4af06d30e39aabb9da271aefba32ab92b842fbf5
chunpuikwan <36138579+chunpuikwan@users.noreply.github.com>
2018-02-12 15:44:57 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 chmod a+x ./src/script.py
 
 # Execute the following command from the root directory
-python ./src/words_tweeted.py ./input/itcont.txt ./input/percentile.txt ./output/repeat_donors.txt
+python ./src/script.py ./input/itcont.txt ./input/percentile.txt ./output/repeat_donors.txt
