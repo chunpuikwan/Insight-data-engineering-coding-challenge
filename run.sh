79fd51abfdfcfebd034c31efccc8ccf289221756
chunpuikwan <36138579+chunpuikwan@users.noreply.github.com>
2018-02-12 16:51:25 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
 # Make sure the python files are executable
 chmod a+x src/script.py
-
 # Execute the following command from the root directory
 python src/script.py input/itcont.txt input/percentile.txt output/repeat_donors.txt
